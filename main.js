bcfd03e5d0934ad011815908639e82f078efd747
pragi <leopragi@kloud.io>
2021-03-23 11:05:04 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const express = require(`express`)
+const PORT = process.env.PORT || 8080;
+const app = express()
+
+app.all(`*`, (req, res) => res.send(`Hello World!`))
+
+app.listen(PORT, () => console.log(`Server listening on ${PORT}`))
