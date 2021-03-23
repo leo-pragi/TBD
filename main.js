d45ca64397c6c0781cf06b14dce7a12d1d61caf8
pragi <leopragi@kloud.io>
2021-03-23 16:44:18 +0530

    3.0

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ const express = require(`express`)
 const PORT = process.env.PORT || 8080;
 const app = express()
 
-app.all(`*`, (req, res) => res.send(`Hello World 2.0!`))
+app.all(`*`, (req, res) => res.send(`Hello World 3.0!`))
 
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`))
